28fee252d66fba3f8e7e2af278b3ce29228dfdc0
Wei Dai <wei@>
2019-11-15 10:49:33 -0800

    continue to do research on project

--- src/main.js
+++ src/main.js
@@ -2,25 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
-
-// Reminder: import functions, classes, etc, from their location
-// Example:
-
-// import { Example, ClassName } from './scripts';
-
-
-
-
-
-
-
-
-
-
-// UI Logic
+import { Earth, Mercury, Venus, Mars, Jupiter } from './scripts';
 
 
 $(document).ready(function() {
 
+  let leonardo = new StrongCharacter("Leonardo", "Twin Katana", "Blue");
+
+    if (donatello.color === "Purple"){
+      $("h1#text-display").append("Hello, folks. Looks like our webpack is working!");
+      $("div#hidden-div").show();
 });
