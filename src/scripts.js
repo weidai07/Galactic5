f55713350e4f16f7df82d4e0f5f42c6fb6598fdf
Wei Dai <wei@>
2019-11-15 15:21:51 -0800

    continue to research build error on project

--- src/scripts.js
+++ src/scripts.js
@@ -2,34 +2,34 @@ import $ from 'jquery';
 
 
 export class Earth {
-  constructor(earthAge){
-    this.earthAge = earthAge;
+  constructor(age){
+    this.age = age;
   }
 }
 
 export class Mercury extends Earth {
-  constructor(mercuryAge) {
+  constructor(age) {
     super(age);
     this.mercuryAge = mercuryAge;
   }
 }
 
 export class Venus extends Earth {
-  constructor(venusAge) {
+  constructor(age) {
     super(age);
     this.venusAge = venusAge;
   }
 }
 
 export class Mars extends Earth {
-  constructor(marsAge) {
+  constructor(age) {
     super(age);
     this.marsAge = marsAge;
   }
 }
 
 export class Jupiter extends Earth {
-  constructor(jupiterAge) {
+  constructor(age) {
     super(age);
     this.jupiterAge = jupiterAge;
   }
